cd3019876061fa64813d9f2f863921d1f4bdee2a
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2025-01-04 23:21:44 +0100

    done with all the designs and logic

--- src/Components/Dashboard/Balance/Balance.jsx
+++ src/Components/Dashboard/Balance/Balance.jsx
@@ -3,6 +3,7 @@ import { fetchTransactions } from '../../../api';
 
 const Balance = () => {
      const [balance, setBalance] = useState(0);
+      const [isVisible, setIsVisible] = useState(true);
     
       
     
@@ -14,28 +15,27 @@ const Balance = () => {
         const response = await fetchTransactions();
         setBalance(response.data.balance);
       };
+
+        const toggleVisibility = () => {
+          setIsVisible(!isVisible);
+        };
   return (
     <div className="py-5 flex justify-between items-center">
-      <div className="flex gap-5">
+      <div className="flex gap-5  items-center">
         <div>
           <h3>Total Balance</h3>
           {/* Format balance with commas */}
-          <p className="font-bold">$ {balance.toLocaleString()}</p>
+          <p className="font-bold">
+            {isVisible ? `$ ${balance.toLocaleString()}` : "****"}
+          </p>
         </div>
-        <svg
-          width="24"
-          height="24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="1.5"
-          viewBox="0 0 24 24"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M2.55 13.406c-.272-.373-.408-.56-.502-.92a2.5 2.5 0 0 1 0-.971c.094-.361.23-.548.502-.92C4.039 8.55 7.303 5 12 5s7.961 3.55 9.45 5.594c.272.373.408.56.502.92a2.5 2.5 0 0 1 0 .971c-.094.361-.23.548-.502.92C19.961 15.45 16.697 19 12 19s-7.961-3.55-9.45-5.594" />
-          <path d="M12 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4" />
-        </svg>
+        <button onClick={toggleVisibility}>
+          {isVisible ? (
+            <i className="fas fa-eye"></i>
+          ) : (
+            <i className="fas fa-eye-slash"></i>
+          )}
+        </button>
       </div>
 
       <svg
